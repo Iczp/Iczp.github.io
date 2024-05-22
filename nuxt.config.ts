91414f2f202334addbf0204e888d715486f0efa2
zhongpei <1000@intry.cn>
2024-05-22 11:24:28 +0800

    files move to 'src/'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  srcDir: 'src/',
+  app: {
+    baseURL: '/',
+    buildAssetsDir: 'static',
+  },
+  experimental: {
+    payloadExtraction: false, // 取消生成 _payload.js
+  },
+});
