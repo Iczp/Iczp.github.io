a350a83881c0ba92c9149e84fbbafde1f1d2c3ed
zhongpei <1000@intry.cn>
2024-06-11 10:32:07 +0800

    000

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -233,9 +233,9 @@ export default defineNuxtConfig({
   //   ],
   // },
   routeRules: {
-    '/': { ssr: true, prerender: true },
-    '/links': { ssr: true, prerender: true },
-    '/about': { ssr: true, prerender: true },
+    '/': { ssr: true, prerender: false },
+    '/links': { ssr: true, prerender: false },
+    '/about': { ssr: true, prerender: false },
     '/notes': { ssr: true, prerender: true },
     '/tags': { ssr: true, prerender: false },
     '/tags/**': { ssr: true, prerender: false },
