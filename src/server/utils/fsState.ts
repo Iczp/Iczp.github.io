0fd79795ce5af17beb1947c0a3260c11faf21fdb
zhongpei <1000@intry.cn>
2024-06-11 10:43:08 +0800

    remove useFileStats

--- src/server/utils/fsState.ts
+++ src/server/utils/fsState.ts
@@ -1,22 +1,22 @@
-import fs from 'fs';
+// import fs from 'fs';
 
-export const useFileStats = (filePath: string) => {
-  try {
-    console.log('filePath', filePath);
-    const stats = fs.statSync(filePath);
+// export const useFileStats = (filePath: string) => {
+//   try {
+//     console.log('filePath', filePath);
+//     const stats = fs.statSync(filePath);
 
-    console.log('File information:');
-    console.log(`File size: ${stats.size} bytes`);
-    console.log(`Last modified: ${stats.mtime}`);
-    console.log(`Created: ${stats.birthtime}`);
+//     console.log('File information:');
+//     console.log(`File size: ${stats.size} bytes`);
+//     console.log(`Last modified: ${stats.mtime}`);
+//     console.log(`Created: ${stats.birthtime}`);
 
-    return {
-      size: stats.size,
-      mtime: stats.mtime,
-      birthtime: stats.birthtime,
-    };
-  } catch (err) {
-    console.error(err);
-    return null;
-  }
-};
+//     return {
+//       size: stats.size,
+//       mtime: stats.mtime,
+//       birthtime: stats.birthtime,
+//     };
+//   } catch (err) {
+//     console.error(err);
+//     return null;
+//   }
+// };
